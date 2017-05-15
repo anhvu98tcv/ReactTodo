d05e12e84014d32428605ac2baa9495497ed98b6
Anh Vu <anhvu869492@gmail.com>
2017-05-15 10:19:52 +0700

    Add completed and crearted

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,8 +1,19 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
   render: function () {
-    var {id, text, completed} = this.props;
+    var {id, text, completed, createAt, completedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created';
+      var timestamp = createAt;
+
+      if (completed) {
+          message = 'Completed';
+          timestamp = completedAt;
+      }
+      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+    };
 
     return (
       <div onClick={() =>{
@@ -10,6 +21,7 @@ var Todo = React.createClass({
         }}>
         <input type="checkbox" checked={completed}/>
         {text}
+        <p>{renderDate()}</p>
       </div>
     )
   }
